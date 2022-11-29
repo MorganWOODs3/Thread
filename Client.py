4fd41fe3f928ff72a446f9bcf464e47ed7a42c80
MorganWOODs3 <92862519+MorganWOODs3@users.noreply.github.com>
2022-11-29 09:48:34 +0100

    création des commandes

--- Client.py
+++ Client.py
@@ -1,16 +1,60 @@
 # echo-client.py
-
+from PyQt5.QtWidgets import QApplication, QWidget, QTextEdit, QVBoxLayout, QPushButton
+import sys
 import socket
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
+if __name__ == '__main__':
+    client_socket = socket.socket()
+    client_socket.connect((HOST, PORT))
+    message = "Client en attente"
+    client_socket.send(message.encode())
+    data = client_socket.recv(1024)
+    print(data.decode())
+    env = input("Envoie une commande: ")
+    client_socket.send(env.encode())
 
-client_socket = socket.socket()
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b"Bonsoir")
-    data = s.recv(1024)
-    host = socket.gethostname()
+    #client_socket.close()
 
-print(f"Received {data!r}")
+
+
+
+# class TextEditDemo(QWidget):
+#     def __init__(self, parent=None):
+#         super().__init__(parent)
+#
+#         self.i = 0
+#         self.setWindowTitle("QTextEdit")
+#         self.resize(300, 270)
+#
+#         self.textEdit = QTextEdit()
+#         self.textEdit.setEnabled(False)
+#         self.btnPress1 = QPushButton("Add message")
+#         self.btnPress2 = QPushButton("Clear")
+#
+#         layout = QVBoxLayout()
+#         layout.addWidget(self.textEdit)
+#         layout.addWidget(self.btnPress1)
+#         layout.addWidget(self.btnPress2)
+#         self.setLayout(layout)
+#
+#         self.btnPress1.clicked.connect(self.btnPress1_Clicked)
+#         self.btnPress2.clicked.connect(self.btnPress2_Clicked)
+#
+#     def btnPress1_Clicked(self):
+#         self.textEdit.append(f"Nouveau texte {self.i}")
+#         self.i += 1
+# #        self.textEdit.setPlainText("Hello PyQt5!\nfrom pythonpyqt.com")
+#
+#     def btnPress2_Clicked(self):
+#         self.textEdit.setPlainText("")
+# #        self.textEdit.setHtml("<font color='red' size='6'><red>Hello PyQt5!\nHello</font>")
+#
+#
+# if __name__ == '__main__':
+#     app = QApplication(sys.argv)
+#     win = TextEditDemo()
+#     win.show()
+#     sys.exit(app.exec_())
--- Server.py
+++ Server.py
@@ -1,27 +1,53 @@
-# echo-server.py
 import socket
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
 
-
 server_socket = socket.socket()
 server_socket.bind(((HOST,PORT)))
 server_socket.listen(1)
 conn, address = server_socket.accept()
-data = conn.recv(1024).decode()
-conn.send(reply.encode())
-conn.close()
+print("J'accept la connection au client depuis %s:%s "% (address[0], address[1]))
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print(f"Connected by {addr}")
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+message = "Server"
+
+
+
+while message != "kill":
+    msg = conn.recv(1024)
+    message = msg.decode()
+    print(message)
+    conn.send("Server connecté ".encode())
+
+
+    if message == "nom":
+        host = socket.gethostname()
+        print(f"Le message vien de : {host}")
+
+    udpSvr = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # if message == "msg":
+    #     rep = input("Envoie un message: ")
+    #     conn.send(rep.encode())
+    #     print(rep)
+    if message == "reset":
+        conn.close()
+        print("Connexion terminé")
+        server_socket = socket.socket()
+        server_socket.bind(((HOST, PORT)))
+        server_socket.listen(1)
+        conn, address = server_socket.accept()
+        print("J'accept la connection au client depuis %s:%s " % (address[0], address[1]))
+
+
+
+conn.close()
+print("Connexion terminé du client")
+server_socket.close()
+print("Connexion terminé du Server")
+
+
+
+
+
